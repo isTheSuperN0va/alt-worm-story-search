121398d4b40bde6d2d4e38bc52d4ec55051170d1
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2026-05-21 13:59:17 -0300

    reformated and added colors to logger

--- backend/logger.ts
+++ backend/logger.ts
@@ -6,11 +6,11 @@ export enum LogSource {
 }
 
 export enum LogType {
-    Error = "Error",
-    Warning = "Warning",
-    Info = "Info"
+    Error = "\x1b[31mError\x1b[0m",
+    Warning = "\x1b[33mWarning\x1b[0m",
+    Info = "\x1b[34mInfo\x1b[0m"
 }
 
-export function error(source: LogSource, message: string) { console.log(`${new Date().toISOString()} - [${source}] ${LogType.Error}: ${message}`); }
-export function info(source: LogSource, message: string) { console.log(` ${new Date().toISOString()} - [${source}] ${LogType.Info}: ${message}`);}
-export function warn(source: LogSource, message: string) { console.log(`${new Date().toISOString()} - [${source}] ${LogType.Warning}: ${message}`);}
+export function error(source: LogSource, message: string) { console.log(`${new Date().toISOString()} - [${source}/${LogType.Error}]: ${message}`); }
+export function info(source: LogSource, message: string) { console.log(` ${new Date().toISOString()} - [${source}/${LogType.Info}]:  ${message}`);}
+export function warn(source: LogSource, message: string) { console.log(`${new Date().toISOString()} - [${source}/${LogType.Warning}] : ${message}`);}
