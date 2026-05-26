4b0c9bd26a482e20df400356e1f10b7d51006e26
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2026-05-26 14:55:31 -0300

    created generalized function for creating table cells with spans

--- frontend/search/app.js
+++ frontend/search/app.js
@@ -1,30 +1,37 @@
 fetch("/api/stories").then((res) => res.json()).then(data => addDataToTable(data));
 
-const table = document.getElementById("searchTable")
+const table = document.getElementById("tableBody")
 
 function addDataToTable(data) {
     for (const row of data) {
-    tableRow = document.createElement("tr");
-    tableRow.classList.add("tableData");
+        tableRow = document.createElement("tr");
+        tableRow.classList.add("tableData");
 
-    let title = document.createElement("td")
-    title.textContent = row.title;
-    let author = document.createElement("td")
-    author.textContent = row.author;
-    let chapters = document.createElement("td")
-    chapters.textContent = row.chapters_released;
-    let updated = document.createElement("td")
-    updated.textContent = row.updated;
-    let fandom = document.createElement("td")
-    fandom.textContent = row.fandom;
+        let title = createTableCell(row.title, "titleData", false);    
+        let author = createTableCell(row.author, "authorData", false);
+        let chapters = createTableCell(row.chapters_released, "chaptersData", true);
+        let updated = createTableCell(row.updated, "updatedData", false);
+        let fandom = createTableCell(row.fandom, "fandomData", false);
 
-    tableRow.appendChild(title);
-    tableRow.appendChild(author);
-    tableRow.appendChild(chapters);
-    tableRow.appendChild(updated);
-    tableRow.appendChild(fandom);
+        tableRow.appendChild(title);
+        tableRow.appendChild(author);
+        tableRow.appendChild(chapters);
+        tableRow.appendChild(updated);
+        tableRow.appendChild(fandom);
 
-    table.appendChild(tableRow)
+        table.appendChild(tableRow)
     }
+}
+
+function createTableCell(data, className, centered) {
+    let cell = document.createElement("td")
+    cell.classList.add(className)
+
+    let cellSpan = document.createElement("span");
+    cellSpan.textContent = data;
+    cell.appendChild(cellSpan)
+    if (centered) cellSpan.classList.add("centered")
+    
+    return cell;
 }
 
