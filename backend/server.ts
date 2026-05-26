8a17b574244ac4982abad23143c684e5c0c81e41
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2026-05-26 11:08:26 -0300

    updated api for new frontend

--- backend/server.ts
+++ backend/server.ts
@@ -14,23 +14,41 @@ Bun.serve({
 
         switch (url.pathname) {
             case "/":
-                return new Response(Bun.file("../frontend/index.html"), {
+                return new Response(Bun.file("../frontend/landing/index.html"), {
                 headers: {
                     "Content-Type":"text/html"
                 }
                 })
             case "/app.js":
-                return new Response(Bun.file("../frontend/app.js"), {
+                return new Response(Bun.file("../frontend/landing/app.js"), {
                 headers: {
                     "Content-Type": "application/javascript"
                 }})
             case "/style.css":
-                return new Response(Bun.file("../frontend/style.css"), {
+                return new Response(Bun.file("../frontend/landing/style.css"), {
                 headers: {
                     "Content-Type": "text/css"
                 }})
+            case "/search":
+                return new Response(Bun.file("../frontend/search/index.html"), {
+                    headers: {
+                        "Content-Type": "text/html"
+                    }
+                })
+            case "/search/style.css":
+                return new Response(Bun.file("../frontend/search/style.css"), {
+                    headers: {
+                        "Content-Type": "text/css"
+                    }
+                })
+            case "/search/app.js":
+                return new Response(Bun.file("../frontend/search/app.js"), {
+                    headers: {
+                        "Content-Type": "application/javascript"
+                    }
+                })
             case "/api/stories":
-                const stories = db.query("SELECT * FROM stories").all();
+                const stories = db.query("SELECT * FROM stories LIMIT 40").all();
                 return Response.json(stories);
             default:
                 return new Response("404", { status: 404 })
@@ -42,6 +60,6 @@ Bun.serve({
 })
 
 console.log(`Listening on port ${portNumber}.`)
-crawler.bootstrap(db)
+// crawler.bootstrap(db)
 // await ao3Crawler.fetchSite(cAo3.ParserAo3.BASE_URL);
 // ao3Crawler.processPage();
